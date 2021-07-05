40995bacdb23535cade73d2490c37712c7898c19
Neil <zooming.in@hotmail.com>
2021-07-06 00:09:45 +1000

    updates

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each {|name| new_array << "Hello, my name is #{name}."}
+    return new_array
+end
+
+def assign_rooms(array)
+    new_array = []
+    array.each_with_index {|speaker, index| new_array << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
+    return new_array
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each {|name| puts name}
+    assign_rooms(attendees).each {|array| puts array}
+end
